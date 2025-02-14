f51d7686377a1075f20d46818f59a4c16ed8ba58
fotox <carsten_stephan@mail.de>
2025-02-14 16:35:31 +0100

    add testcase for empty user

--- tests/common/test_schemas.py
+++ tests/common/test_schemas.py
@@ -7,7 +7,7 @@ from ..helper.load_test_data import load_test_data
 
 @pytest.mark.parametrize("test_data", load_test_data("test_schema"))
 def test_message(test_data):
-    if type(test_data['input']['sender']) and isinstance(test_data['input']['message'], str):
+    if isinstance(test_data['input']['sender'], str) and isinstance(test_data['input']['message'], str):
         msg = Message(sender=test_data['input']['sender'], message=test_data['input']['message'])
         assert msg.sender == test_data['expected']['sender']
         assert msg.message == test_data['expected']['message']
