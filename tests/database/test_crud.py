9f3e001595ae5e957f7714b99b0d99d5e70f9443
fotox <carsten_stephan@mail.de>
2025-02-15 22:52:54 +0100

    rescale crud tests

--- tests/database/test_crud.py
+++ tests/database/test_crud.py
@@ -12,24 +12,6 @@ def test_get_or_create_user(test_data):
     mock_connect = MagicMock()
     mock_cursor = MagicMock()
 
-    mock_cursor.fetchone.side_effect = [test_data["expected"],
-                                        None] if test_data["expected"] else [None, test_data["expected"]]
-
-    mock_connect.cursor.return_value = mock_cursor
-    mock_connect.commit = MagicMock()
-
-    result = get_or_create_user(test_data['input']['username'], test_data['input']['role'], mock_connect)
-
-    assert result["username"] == test_data["expected"]["username"]
-    assert result["id"] == test_data["expected"]["id"]
-    assert mock_cursor.fetchone.called
-
-
-@pytest.mark.parametrize("test_data", load_test_data("test_crud_get_or_create_user"))
-def test_get_or_create_user_inserts_new_user(test_data):
-    mock_connect = MagicMock()
-    mock_cursor = MagicMock()
-
     mock_cursor.fetchone.side_effect = [None, test_data["expected"]]
 
     mock_connect.cursor.return_value = mock_cursor
@@ -51,29 +33,6 @@ def test_get_or_create_user_inserts_new_user(test_data):
 
 @pytest.mark.parametrize("test_data", load_test_data("test_crud_get_or_create_chat"))
 def test_get_or_create_chat(test_data):
-    mock_connect = MagicMock()
-    mock_cursor = MagicMock()
-
-    mock_cursor.fetchone.side_effect = [None, test_data["expected"]]
-
-    mock_connect.cursor.return_value = mock_cursor
-    mock_connect.commit = MagicMock()
-
-    result = get_or_create_chat(test_data['input']['user_id'], mock_connect)
-
-    assert result is not None
-    assert result["id"] == test_data["expected"]["id"]
-
-    mock_cursor.execute.assert_any_call(
-        "INSERT INTO chats (user_id, id) VALUES (%s, %s) RETURNING id;",
-        (test_data["input"]["user_id"], ANY)
-    )
-
-    mock_connect.commit.assert_called_once()
-
-
-@pytest.mark.parametrize("test_data", load_test_data("test_crud_get_or_create_chat"))
-def test_get_or_create_chat_inserts_new_chat(test_data):
     mock_connect = MagicMock()
     mock_cursor = MagicMock()
 
