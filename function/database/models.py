9205900c68ae2f8f896f15515ab9b83b061be2f2
fotox <carsten_stephan@mail.de>
2025-02-15 22:52:14 +0100

    bug fixed connection error

--- function/database/models.py
+++ function/database/models.py
@@ -1,26 +1,25 @@
 import psycopg2
 
+from database.connection import get_db_connection
 from log_module import __init_log_module
-from .crud import get_connection_to_database
 
 logging = __init_log_module('server')
 
 
-def create_tables(conn=get_connection_to_database()) -> None:
+def create_tables(conn=None) -> None:
     """
     Creates all tables in the database.
     :return: None
     """
     if conn is None:
-        logging.error("No connection to the database possible.")
-        return None
+        conn = get_db_connection()
 
     try:
         cur: psycopg2.extensions.cursor = conn.cursor()
 
         cur.execute("""
             CREATE TABLE IF NOT EXISTS users (
-                id TEXT PRIMARY KEY UNIQUE NOT NULL DEFAULT gen_random_uuid(),
+                id TEXT PRIMARY KEY UNIQUE NOT NULL,
                 username TEXT UNIQUE NOT NULL,
                 role TEXT CHECK (role IN ('user', 'supporter')) NOT NULL
             );
@@ -29,9 +28,9 @@ def create_tables(conn=get_connection_to_database()) -> None:
 
         cur.execute("""
             CREATE TABLE IF NOT EXISTS chats (
-                id TEXT PRIMARY KEY UNIQUE NOT NULL DEFAULT gen_random_uuid(),
-                user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-                supporter_id INTEGER REFERENCES users(id) ON DELETE SET NULL,
+                id TEXT PRIMARY KEY UNIQUE NOT NULL,
+                user_id TEXT REFERENCES users(id) ON DELETE CASCADE,
+                supporter_id TEXT REFERENCES users(id) ON DELETE SET NULL,
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             );
         """)
@@ -41,7 +40,7 @@ def create_tables(conn=get_connection_to_database()) -> None:
             CREATE TABLE IF NOT EXISTS messages (
                 id SERIAL PRIMARY KEY,
                 chat_id TEXT REFERENCES chats(id) ON DELETE CASCADE,
-                sender_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+                sender_id TEXT REFERENCES users(id) ON DELETE CASCADE,
                 message TEXT NOT NULL,
                 timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             );
